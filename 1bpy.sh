d06462d528964660973efe6e477796bf06de38f9
JKiddu <karanapatel9661@gmail.com>
2019-05-24 02:59:13 -0700

    Assignment 1 Question 1 & 2 commit

---
+++ 1bpy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('/home/hillary/cpsc4810/flightdelays.csv')
+print(df[df['Cancelled']==0]['Dest'].value_counts().head(3))
+
