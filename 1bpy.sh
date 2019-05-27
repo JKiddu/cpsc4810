5561126a60dc7cc68338bac2d9ead9d40590cc12
JKiddu <51060838+JKiddu@users.noreply.github.com>
2019-05-26 20:52:18 -0700

    Delete 1bpy.sh

--- 1bpy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-import numpy as np
-import pandas as pd
-
-df = pd.read_csv('/home/hillary/cpsc4810/flightdelays.csv')
-print(df[df['Cancelled']==0]['Dest'].value_counts().head(3))
